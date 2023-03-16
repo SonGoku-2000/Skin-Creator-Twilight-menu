80e439ad24b85daf4d3274101fa22258a46f2934
SonGoku-2000 <pablontxs@gmail.com>
2023-03-16 18:05:49 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from PIL import Image
+
+nombre = 'bottom_moving'
+with Image.open(nombre + ".png" ) as image:
+    with image.quantize(colors=256, method=2) as converted:
+        converted.save(nombre+'1.png')
