eacdc2b19b318aa9e0f55bc8e4dcbc5bfd13fe23
SonGoku-2000 <pablontxs@gmail.com>
2023-03-18 12:05:20 -0500

    Agregado crear paquete de skin

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 #!/usr/bin/env python3
 
 from PIL import Image
+from pathlib import Path
 
 carpetaImagenes = "Imagenes/"
-carpetaPlantillas = "Plantillas/"
-carpetaPruebas = "Pruebas/"
-carpetaDestino = "Destino/"
-nombre = "gine.jpg"
+carpetaPlantillas = carpetaImagenes + "Plantillas/"
+carpetaPruebas = carpetaImagenes + "Pruebas/"
+carpetaSkins = "Skins/"
 
 
-def guardarImagen(nombreImagen: str, nombreSalida: str = ""):
-    if(nombreSalida == ""):
-        nombreSalida = nombreImagen
-    tamanoImagen = (256,192)
-    with Image.open(carpetaImagenes + carpetaPruebas + nombre) as image:
+def guardarImagen(dirImagen: str, dirSalida: str, nombreSalida: str):
+    tamanoImagen = (256, 192)
+    Path(dirSalida).mkdir(parents=True, exist_ok=True)
+    with Image.open(dirImagen) as image:
         image = image.resize(tamanoImagen)
         with image.quantize(colors=256, method=2) as converted:
-            converted.save(carpetaImagenes + carpetaDestino + nombreSalida)
+            converted.save(dirSalida + nombreSalida)
+
+
+def crearPaquete(imagenBase: str, nombrePaquete: str = "white", paqueteBase: str = ""):
+    carpetaSkin = Path(carpetaSkins + nombrePaquete + "/")
+    carpetaSkin.mkdir(parents=True, exist_ok=True)
+    guardarImagen(dirImagenBase, carpetaSkin.__str__() +
+                  "/background/", "bottom.png")
+    guardarImagen(dirImagenBase, carpetaSkin.__str__() +
+                  "/background/", "bottom_bubble.png")
+    guardarImagen(dirImagenBase, carpetaSkin.__str__() +
+                  "/background/", "bottom_bubble_macro.png")
+    guardarImagen(dirImagenBase, carpetaSkin.__str__() +
+                  "/background/", "bottom_moving.png")
+
 
 if __name__ == "__main__":
-    guardarImagen(nombre,"bottom.png")
-    guardarImagen(nombre,"bottom_bubble.png")
-    guardarImagen(nombre,"bottom_bubble_macro.png")
-    guardarImagen(nombre,"bottom_moving.png")
+    dirImagenBase = carpetaPlantillas + "gine.jpg"
+    crearPaquete(dirImagenBase, "test1")
