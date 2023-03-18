47f7e05148011dd4933192dc1106bf7ddfda3042
SonGoku-2000 <pablontxs@gmail.com>
2023-03-18 15:06:11 -0500

    funcion imagenBottom agregada

--- main.py
+++ main.py
@@ -39,11 +39,27 @@ def guardarImagen(dirImagen: str, dirSalida: str, nombreSalida: str):
             converted.save(dirSalida + nombreSalida)
 
 
-def crearPaquete(imagenBase: str, nombrePaquete: str = "white", paqueteBase: str = ""):
+def imagenBottom(dirImagenBase: str, carpetaSkin: Path):
+    temp = Path(".temp")
+    temp.mkdir(parents=True, exist_ok=True)
+    dirImagenTemp = temp.joinpath("temp.png")
+
+    with Image.open(dirImagenBase) as imagenBase:
+        reescalada = imagenBase.resize((256, 192))
+        with Image.open(carpetaPlantillas + "white/background/bottom.png") as bottom:
+            reescalada.paste(bottom, (0, 0), bottom)
+        reescalada.save(dirImagenTemp)
+
+    guardarImagen(dirImagenTemp.__str__(), carpetaSkin.__str__() +
+                  "/background/", "bottom.png")
+    dirImagenTemp.unlink()
+    temp.rmdir()
+
+
+def crearPaquete(dirImagenBase: str, nombrePaquete: str = "white", paqueteBase: str = ""):
     carpetaSkin = Path(carpetaSkins + nombrePaquete + "/")
     carpetaSkin.mkdir(parents=True, exist_ok=True)
-    guardarImagen(dirImagenBase, carpetaSkin.__str__() +
-                  "/background/", "bottom.png")
+    imagenBottom(dirImagenBase, carpetaSkin)
     guardarImagen(dirImagenBase, carpetaSkin.__str__() +
                   "/background/", "bottom_bubble.png")
     guardarImagen(dirImagenBase, carpetaSkin.__str__() +
