fb7e36ec732d8a8b494a68bfff5449623ff4839c
SonGoku-2000 <pablontxs@gmail.com>
2023-03-18 07:46:40 -0500

    Agregado directorio para imagenes

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 from PIL import Image
 
-nombre = 'bottom_moving'
-with Image.open(nombre + ".png" ) as image:
+carpeta = "Imagenes/"
+nombre = "bottom_moving"
+with Image.open(carpeta + nombre + ".png") as image:
     with image.quantize(colors=256, method=2) as converted:
-        converted.save(nombre+'1.png')
+        converted.save(carpeta + nombre + "1.png")
