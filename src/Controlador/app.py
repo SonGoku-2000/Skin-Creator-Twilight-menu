a8870949ee0764555b797ad52088de7e3a23acc4
SonGoku-2000 <pablontxs@gmail.com>
2023-03-19 15:44:22 -0500

    funciones a botones secImagen y crearSkin agregada

--- src/Controlador/app.py
+++ src/Controlador/app.py
@@ -5,24 +5,33 @@ from src.Modelo.modelo import modelo
 from src.Vista.ventana_principal import ventanaPrincipal
 
 
-
 class Controller():
     def __init__(self):
         self.root = Tk.Tk()
         self.root.title("Skin Creator for TWiLight Menu++")
         self.root.deiconify()
-        
+
         self.modelo = modelo()
 
         self.view = ventanaPrincipal(self.root)
-        self.view.panelLateral.botonSelectImagen.bind("<Button>", self.seleccionarImagen)
+        self.view.panelLateral.botonSelectImagen.bind(
+            "<Button>", self.seleccionarImagen)
         self.view.panelLateral.botonCrearSkin.bind("<Button>", self.crearSkin)
 
     def run(self):
         self.root.mainloop()
-    
-    def seleccionarImagen(self,event):
-        pass
 
-    def crearSkin(self,event):
-        pass
+    def seleccionarImagen(self, event):
+        self.modelo.seleccionarImagen()
+        if self.modelo.dirImagen != "":
+            self.view.panelLateral.activarBotonCrearSkin()
+        else:
+            self.view.panelLateral.desactivarBotonCrearSkin()
+        self.root.mainloop()
+
+
+    def crearSkin(self, event):
+        self.modelo.crearSkin()
+
+if __name__ == "__main__":
+    pass
