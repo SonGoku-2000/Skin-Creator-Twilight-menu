6b7177db62e2a79bf9eb8b2078090149d9d44f37
SonGoku-2000 <pablontxs@gmail.com>
2023-03-19 11:46:23 -0500

    Metodo para crear ImagenBottom creado en modelo.py

--- src/Modelo/modelo.py
+++ src/Modelo/modelo.py
@@ -9,8 +9,9 @@ class modelo():
         self.CARPETA_IMAGENES = Path("Imagenes")
         self.CARPETA_PLANTILLAS = self.CARPETA_IMAGENES.joinpath("Plantillas")
         self.CARPETA_SKINS = Path("Skins")
-        self.dirImagen = ""
+
         self.TAMANO_A_CONVERTIR = (256, 192)
+        self.dirImagen = ""
 
     def __selectorImagen(self, dirBase: str = '.') -> str:
         """
@@ -43,7 +44,7 @@ class modelo():
         nombreSalida : str
             Nombre que va a tener la imagen en la carpeta de destino.
         """
-        
+
         dirSalida.mkdir(parents=True, exist_ok=True)
         with Image.open(dirImagen) as image:
             image = image.resize(self.TAMANO_A_CONVERTIR)
@@ -53,8 +54,54 @@ class modelo():
     def seleccionarImagen(self):
         self.dirImagen = self.__selectorImagen()
 
-    def crearSkin(self):
-        pass
+    def __imagenBottom(self, dirImagenBase: Path, carpetaSkin: Path):
+        """
+        Parameters
+        ----------
+        dirImagenBase : Path
+            Directorio que de la imagen que se usara como base para el paquete.
+
+        carpetaSkin : Path
+            Carpeta de la skin en donde se van a guardar la imagen bottom.
+        """
+
+        temp = Path(".temp")
+        temp.mkdir(parents=True, exist_ok=True)
+        dirImagenTemp = temp.joinpath("temp.png")
+
+        dirImagenDecoracion = self.CARPETA_PLANTILLAS.joinpath(
+            "white/background/bottom.png")
+
+        with Image.open(dirImagenBase) as imagenBase:
+            imagenBase = imagenBase.resize(self.TAMANO_A_CONVERTIR)
+
+            with Image.open(dirImagenDecoracion) as bottom:
+                imagenBase.paste(bottom, (0, 0), bottom)
+            imagenBase.save(dirImagenTemp)
+
+        self.__guardarImagen(dirImagenTemp, carpetaSkin.joinpath("background"),
+                             "bottom.png")
+        dirImagenTemp.unlink()
+        temp.rmdir()
+
+    def crearSkin(self, dirImagenBase: Path, nombrePaquete: str = "white", paqueteBase: str = "white"):
+        """
+        Parameters
+        ----------
+        dirImagenBase : str
+            Directorio que de la imagen que se usara como base para el paquete.
+
+        nombrePaquete : str, default "white"
+            Nombre que se le dara al paquete creado.
+
+        paqueteBase : str, default "white"
+            Nombre de la decoracion base que se usa para crear el paquete puede ser "white" o "dark".
+        """
+
+        carpetaSkin = self.CARPETA_SKINS.joinpath(nombrePaquete)
+        carpetaSkin.mkdir(parents=True, exist_ok=True)
+
+        self.__imagenBottom(dirImagenBase, carpetaSkin)
 
 
 if __name__ == '__main__':
